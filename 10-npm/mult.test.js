49a8c05324fd45af8baa973e147b3993d305422e
LexAV78 <gofride@gmail.com>
2023-06-20 21:08:57 +0300

    module env

---
+++ 10-npm/mult.test.js
@@ -0,0 +1,4 @@
+const mult = require('./mult')
+test('5*10=50',()=>{
+    expect(mult(5,10)).toBe(50)
+})
---
+++ 11-env/index.js
@@ -0,0 +1,4 @@
+import * as dotenv from 'dotenv'
+console.log('1:',process.env.lex_env)
+dotenv.config()
+console.log('2:',process.env.lex_env)
